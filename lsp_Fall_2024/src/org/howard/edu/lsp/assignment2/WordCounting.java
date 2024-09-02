1f99728441e1185575143e277da79e63bd961db6
Codeo <toa9953@nyu.edu>
2024-09-02 18:09:14 -0400

    Initial Commit with Word Counting algorithm

---
+++ lsp_Fall_2024/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -0,0 +1,49 @@
+package org.howard.edu.lsp.assignment2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class WordCounting {
+
+    public static void main(String[] args) {
+        // Input relative path
+        String filePath = "src/org/howard/edu/lsp/assignment2/word.txt";
+        
+        // Store word counts while maintaining insertion order - LinkedHashMap
+        Map<String, Integer> wordCounts = new LinkedHashMap<>();
+
+        // Try catch block to read file 
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                // Split the line into words 
+                String[] wordsInLine = line.split("[^a-zA-Z]+");
+                for (String word : wordsInLine) {
+                    if (!word.isEmpty() && !isaTrivWord(word) && !isaNum(word)) {
+                    	// Change word to Lowercase
+                        word = word.toLowerCase(); 
+                        wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("OOOOps error reading file: " + e.getMessage());
+        }
+
+        // Display the word counts
+        wordCounts.forEach((word, count) -> System.out.println(word + " " + count));
+    }
+
+    // Helper to find numeric words
+    private static boolean isaNum(String str) {
+        return str.chars().allMatch(Character::isDigit);
+    }
+
+    // Helper to find trivial words
+    private static boolean isaTrivWord(String word) {
+        return word.length() <= 3;
+    }
+}
